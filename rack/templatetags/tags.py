9dbb5d5abe350c82106f85428d197e3dbc2a71ba
alexey <a.parshin@comearth.ru>
2022-08-01 18:01:11 +0300

    функционал формирования формы для создания заметки для сервера вынесен в функцию формирования контекста RackCreateView
    Миграции удалены из списка gitignore

--- rack/templatetags/tags.py
+++ rack/templatetags/tags.py
@@ -6,15 +6,10 @@ register = template.Library()
 
 
 @register.filter
-def range_(number):
+def get_range(number):
 	return range(number)
 
 
 @register.simple_tag
-def index_(lst, index):
+def get_item(lst, index):
 	return lst[index] if index < len(lst) else False
-
-
-@register.inclusion_tag('rack/note.html')
-def get_note_form(obj):
-	return {'object': obj, 'form_note': ServerNoteForm(instance=obj)}
