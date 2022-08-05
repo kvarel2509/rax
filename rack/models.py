538ce63932675a85304f107694b0a54af77f9d44
alexey <a.parshin@comearth.ru>
2022-08-05 14:53:11 +0300

    v2

--- rack/models.py
+++ rack/models.py
@@ -18,12 +18,33 @@ class Rack(models.Model):
 		return reverse('rack_detail', kwargs={'pk': self.pk})
 
 
+class Port(models.Model):
+	SPEED_CHOICES = [('10Mbit', '10Mbit'), ('100Mbit', '100Mbit'), ('1Gbit', '1Gbit'), ('10Gbit', '10Gbit')]
+	MATERIAL_CHOICES = [('Медь', 'Медь'), ('Оптика', 'Оптика')]
+
+	note = models.TextField('Комментарий', blank=True, null=True)
+	color = models.CharField('Цвет', max_length=20)
+	speed = models.CharField('Скорость порта', choices=SPEED_CHOICES, max_length=20)
+	material = models.CharField('Тип порта', choices=MATERIAL_CHOICES, max_length=20)
+	server = models.ForeignKey('Server', on_delete=models.CASCADE)
+	connection = models.TextField('Объект на том конце', blank=True, null=True)
+
+	class Meta:
+		verbose_name = 'Порт'
+		verbose_name_plural = 'Порты'
+
+	def __str__(self):
+		return f'{self.pk} - {self.speed}, {self.material}'
+
+
 class Server(models.Model):
 	title = models.CharField('Метка сервера', max_length=50)
 	length = models.PositiveIntegerField('Размер')
 	note = models.TextField('Заметки', blank=True, null=True)
 	rack = models.ForeignKey(Rack, on_delete=models.SET_NULL, null=True)
 	color = models.CharField('Цвет', max_length=50)
+	base_speed = models.CharField('Скорость порта', choices=Port.SPEED_CHOICES, max_length=20)
+	base_material = models.CharField('Тип порта', choices=Port.MATERIAL_CHOICES, max_length=20)
 
 	class Meta:
 		verbose_name = 'Сервер'
@@ -31,3 +52,6 @@ class Server(models.Model):
 
 	def __str__(self):
 		return self.title
+
+	def get_absolute_url(self):
+		return reverse('server_detail', kwargs={'pk': self.pk})
